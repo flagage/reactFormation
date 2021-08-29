d2d47ef9580aaef4ac2473152da75fe785868a7a
fred <flagage@gmail.com>
2021-08-29 15:33:37 +0200

    don't forget the code

---
+++ sources/rts/events/EventComponent.tsx
@@ -0,0 +1,24 @@
+const EventComponent: React.FC= () => {
+
+    const onChange=(event: React.ChangeEvent<HTMLInputElement>) => {
+        console.log(event);
+    };
+    const onDragStart=(event: React.DragEvent<HTMLDivElement>) => {
+        console.log(event);
+    };
+    return (<div>
+        <input onChange={onChange} />
+        <div draggable onDragStart={onDragStart}>DragMe!</div>
+    </div>
+);
+};
+
+export default EventComponent;
+
+
+
+
+
+
+
+
